d4e5d18d7f9ec82a4c8b6b925039d422acb46437
Skalmarrr <mateuszkal2002@wp.pl>
2026-04-15 23:07:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var lab1_1 = require("./lab/lab1_1").lab
+// 1. Importy modułów
+var lab1_1 = require("./lab/lab1_1").lab;
 var example_1 = require("./example_1").lab;
+var ec2_info = require("./lab/ec2_info").lab; // Przeniesione tutaj
 
 var PORT = 8080;
 
-
+// 2. Definicja mapy URL (tutaj musisz dopisać nową ścieżkę!)
 var urlMap = [
-	{path: "/", action:__dirname + "/static/index.html"},	 
-	{path: "/digest", action: lab1_1},	
-	{path: "/example_1", action: example_1}, 
-	];
+    {path: "/", action: __dirname + "/static/index.html"},
+    {path: "/digest", action: lab1_1},
+    {path: "/example_1", action: example_1},
+    {path: "/ec2info", action: ec2_info} // Dodane do tablicy
+];
 
+// 3. Uruchomienie usługi z kompletną mapą URL
 var service = require("./lib/service").http(urlMap);
-var ec2_info = require("./lab/ec2_info").lab;
 
 service(PORT);
-
